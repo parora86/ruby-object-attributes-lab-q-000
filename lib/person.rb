b27cfd4e2c901a16987d233ca0fb4194c6d47cde
parora86 <parora.cpa@outlook.com>
2016-01-03 03:56:28 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name=(name_string)
+    @name = name_string
+  end
+
+  def job=(job_string)
+    @job = job_string
+  end
+  
+  def job
+    @job
+  end
+  
+  def name
+    @name
+  end
+  
+end
